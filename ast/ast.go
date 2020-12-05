1240b034d4d1464c953c66602d51e2a72b65d80c
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2020-12-05 18:25:43 +0900

    let statement

---
+++ ast/ast.go
@@ -0,0 +1,48 @@
+package ast
+
+import "github.com/shunsukesan/monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+// Statement (文)
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Expression (文)
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+
+	return ""
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
