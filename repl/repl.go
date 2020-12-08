53489ee4e6091a1083dde899ffce44c75a589269
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2020-12-09 00:16:47 +0900

    object & evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/shunsukesan/monkey/evaluator"
 	"github.com/shunsukesan/monkey/lexer"
 	"github.com/shunsukesan/monkey/parser"
 )
@@ -32,8 +33,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
